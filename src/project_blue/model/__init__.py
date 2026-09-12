74947354b63acf6f59ffb8f3abac6d562e326f29
justgodtears <oopyrchal@pm.me>
2026-09-12 14:02:20 +0200

    added single head attention to learn about it and mha on the way

--- src/project_blue/model/__init__.py
+++ src/project_blue/model/__init__.py
@@ -12,3 +12,28 @@ class TokenAndPositionEmbedding(nn.Module):
         position_tensor = torch.arange(token_ids.shape[1])
         position_embeddings = self.position_embedding(position_tensor)
         return token_embeddings + position_embeddings
+
+# It was only used to help me learn the Single-Head architecture
+# all new GPTs use Multi-Head, so this class is not used
+class SelfAttention(nn.Module):
+    def __init__(self, embed_dim: int):
+        super().__init__()
+        self.embed_dim = embed_dim
+        self.query_layer = nn.Linear(embed_dim, embed_dim)
+        self.key_layer = nn.Linear(embed_dim, embed_dim)
+        self.value_layer = nn.Linear(embed_dim, embed_dim)
+
+    def forward(self, x):
+        Q = self.query_layer(x)
+        K = self.key_layer(x)
+        V = self.value_layer(x)
+        attention_scores = torch.matmul(Q, K.transpose(-2, -1))
+        scaled = attention_scores / (self.embed_dim ** 0.5)
+        softmax = torch.softmax(scaled, dim=-1)
+        weighted_sum = torch.matmul(softmax, V)
+        return weighted_sum
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, embed_dim: int, num_heads: int):
+        super().__init__()
